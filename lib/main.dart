28384ca0a4491e46916dbada2ed53a60f3848f80
junghee.kim <wjdgml3834@naver.com>
2022-04-15 02:42:26 +0900

    ➕이미지 파일,아이콘 파일 추가 및 등록

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('앱제목')),
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-          child: Text('하단바'),
-        ) ,
-      )
+      debugShowCheckedModeBanner: false,
+      title: '교양 프로젝트',
+      home: LogIn()
+    );
+  }
+}
+
+
+class LogIn extends StatefulWidget {
+  const LogIn({Key? key}) : super(key: key);
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Container()
+
     );
   }
 }
