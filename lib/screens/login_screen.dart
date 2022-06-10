eee154f93b33c27e8df20172aa90c2273306e8af
Your Name <wjdgml3834@naver.com>
2022-06-10 18:54:18 +0900

    ♻️Refacotr: 로그인화면, 회원가입화면 버튼 컴포넌트로 대체

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hanyang_flutter_project/components/rounded_button.dart';
 
 class LoginScreen extends StatefulWidget {
 
@@ -78,24 +79,8 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement login functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(title: '로그인', colour: Color(0xFF8AA2A0),
+                onPressed: (){})
           ],
         ),
       ),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hanyang_flutter_project/components/rounded_button.dart'
 
 class RegistrationScreen extends StatefulWidget {
 
@@ -74,25 +75,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement registration functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
+           RoundedButton(title: '회원가입', colour: Color(0xFF8AA2A0), onPressed: (){})
           ],
         ),
       ),
